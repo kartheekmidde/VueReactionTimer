7f135f0bb4b84fb200f74db21a893589c4912240
Kartheek Midde <car621@gmail.com>
2021-03-07 19:53:51 -0500

    Timer changes done

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time: {{ this.reactionTime }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["reactionTime"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.reactionTime < 250) {
+      this.rank = "Super fingers";
+    } else if (this.reactionTime < 400) {
+      this.rank = "Rapid";
+    } else {
+      this.rank = "Slow";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
